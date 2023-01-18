cbeac3326a2c707067d864aea06b287c18a21ea0
Nandar Sukmana <nsukmana.dev@gmail.com>
2023-01-18 09:18:59 +0700

    test handler serve mux

--- handler_test.go
+++ handler_test.go
@@ -21,3 +21,29 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello Index")
+	})
+	mux.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hi")
+	})
+	mux.HandleFunc("/image", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Image")
+	})
+	mux.HandleFunc("/image/thumbnail", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Thumbnail")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8790",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
