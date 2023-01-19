2ba987965237dd00eb2562d3d66428e0416dc4cd
Nandar Sukmana <nsukmana.dev@gmail.com>
2023-01-19 08:45:52 +0700

    query parameter

---
+++ query_param_test.go
@@ -0,0 +1,49 @@
+package golangweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SayHello(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		fmt.Fprint(w, "Hello")
+	} else {
+		fmt.Fprintf(w, "Hello %s", name)
+	}
+}
+
+func TestQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8799/hello?name=Nandar", nil)
+	recorder := httptest.NewRecorder()
+
+	SayHello(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+func MultipleQueryParameter(w http.ResponseWriter, r *http.Request) {
+	firstName := r.URL.Query().Get("first_name")
+	lastname := r.URL.Query().Get("last_name")
+
+	fmt.Fprintf(w, "Hello %s %s", firstName, lastname)
+}
+
+func TestMultipleQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8799/hello?first_name=Nandar&last_name=Sukmana", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleQueryParameter(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
